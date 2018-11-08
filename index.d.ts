a9469cdb0422bf5ce51a7feed2e2db2299d8dd30
Alvis HT Tang <alvis@hilbert.space>
2018-11-08 16:40:45 +0000

    feat: add typescript definition

---
+++ index.d.ts
@@ -0,0 +1,63 @@
+declare namespace tarstream {
+  interface Header {
+    name: string
+    size: number
+    mode: number
+    mtime: Date
+    type: string
+    linkname: string
+    uid: number
+    gid: number
+    uname: string
+    gname: string
+    devmajor: number
+    devminor: number
+    pax?: object
+  }
+
+  type HeaderForPack = Partial<Header> & {
+    name: string
+  }
+
+  interface Pack extends NodeJS.ReadableStream {
+    entry(header: HeaderForPack, callback?: (error?: Error) => void): Sink
+    entry(
+      header: HeaderForPack,
+      buffer: string | Buffer,
+      callback?: (error?: Error) => void
+    ): Sink
+    destroy(): void
+    finalize(): void
+  }
+
+  interface Sink extends NodeJS.WritableStream {
+    destroy(): void
+  }
+
+  interface ExtractOptions {
+    filenameEncoding?: string
+  }
+
+  interface Extract extends NodeJS.WritableStream {
+    on(
+      event: 'entry',
+      callback: (
+        header: Header,
+        stream: NodeJS.ReadableStream,
+        next: () => void
+      ) => void
+    ): void
+    on(event: 'close', callback: () => void)
+    on(event: 'finish', callback: () => void)
+    on(event: 'error', callback: (error: Error) => void)
+    destroy(error?: Error): void
+  }
+
+  interface Module {
+    pack(): Pack
+    extract(options?: ExtractOptions): Extract
+  }
+
+  export function pack(): Pack
+  export function extract(options?: ExtractOptions): Extract
+}
