8b67d8d49f62feee7101e1fecc6175c671152081
Mathias Buus <mathiasbuus@gmail.com>
2023-06-17 13:30:02 +0200

    refactor pack and allow multiple entry streams

--- pack.js
+++ pack.js
@@ -1,6 +1,5 @@
 const { constants } = require('fs')
-const { Readable, Writable } = require('streamx')
-const { StringDecoder } = require('string_decoder')
+const { Readable, Writable, getStreamError } = require('streamx')
 const b4a = require('b4a')
 
 const headers = require('./headers')
@@ -10,55 +9,97 @@ const FMODE = 0o644
 
 const END_OF_TAR = b4a.alloc(1024)
 
-const noop = function () {}
-
-const overflow = function (self, size) {
-  size &= 511
-  if (size) self.push(END_OF_TAR.subarray(0, 512 - size))
-}
-
-function modeToType (mode) {
-  switch (mode & constants.S_IFMT) {
-    case constants.S_IFBLK: return 'block-device'
-    case constants.S_IFCHR: return 'character-device'
-    case constants.S_IFDIR: return 'directory'
-    case constants.S_IFIFO: return 'fifo'
-    case constants.S_IFLNK: return 'symlink'
-  }
-
-  return 'file'
-}
-
 class Sink extends Writable {
-  constructor (to) {
-    super()
+  constructor (pack, header, callback) {
+    super({ mapWritable })
+
     this.written = 0
-    this._to = to
+    this.header = header
+
+    this._callback = callback
+    this._linkname = null
+    this._isLinkname = header.type === 'symlink' && !header.linkname
+    this._isVoid = header.type !== 'file' && header.type !== 'contiguous-file'
+    this._finished = false
+    this._pack = pack
+    this._openCallback = null
+
+    if (this._pack._stream === null) this._pack._stream = this
+    else this._pack._pending.push(this)
+  }
+
+  _open (cb) {
+    this._openCallback = cb
+    if (this._pack._stream === this) this._continueOpen()
+  }
+
+  _continueOpen () {
+    if (this._pack._stream === null) this._pack._stream = this
+
+    const cb = this._openCallback
+    this._openCallback = null
+    if (cb === null) return
+
+    if (this._pack.destroying) return cb(new Error('pack stream destroyed'))
+    if (this._pack._finalized) return cb(new Error('pack stream is already finalized'))
+
+    this._pack._stream = this
+
+    if (!this._isLinkname) {
+      this._pack._encode(this.header)
+    }
+
+    cb(null)
   }
 
   _write (data, cb) {
+    if (this._isLinkname) {
+      this._linkname = this._linkname ? b4a.concat([this._linkname, data]) : data
+      return cb(null)
+    }
+
+    if (this._isVoid) {
+      return cb(new Error('No body allowed for this entry'))
+    }
+
     this.written += data.byteLength
-    if (this._to.push(data)) return cb()
-    this._to._drain = cb
-  }
-}
-
-class LinkSink extends Writable {
-  constructor () {
-    super()
-    this.linkname = ''
-    this._decoder = new StringDecoder('utf-8')
+    if (this._pack.push(data)) return cb()
+    this._pack._drain = cb
   }
 
-  _write (data, cb) {
-    this.linkname += this._decoder.write(data)
+  _final (cb) {
+    if (this._isLinkname) {
+      this.header.linkname = this._linkname ? b4a.toString(this._linkname, 'utf-8') : ''
+      this._pack._encode(this.header)
+    }
+
+    overflow(this._pack, this.header.size)
+
+    if (this.written !== this.header.size) { // corrupting tar
+      return cb(new Error('Size mismatch'))
+    }
+
+    this._pack._done(this)
+    this._finished = true
+
+    cb(null)
+  }
+
+  _getError () {
+    return getStreamError(this) || new Error('tar entry destroyed')
+  }
+
+  _predestroy () {
+    this._pack.destroy(this._getError())
+  }
+
+  _destroy (cb) {
+    this._pack._done(this)
+
+    if (this._finished) this._callback(null)
+    else this._callback(this._getError())
+
     cb()
-  }
-}
-
-class Void extends Writable {
-  _write (data, cb) {
-    cb(new Error('No body allowed for this entry'))
   }
 }
 
@@ -68,12 +109,12 @@ class Pack extends Readable {
     this._drain = noop
     this._finalized = false
     this._finalizing = false
+    this._pending = []
     this._stream = null
   }
 
   entry (header, buffer, callback) {
-    if (this._stream) throw new Error('already piping an entry')
-    if (this._finalized || this.destroyed) return
+    if (this._finalized || this.destroying) throw new Error('already finalized or destroyed')
 
     if (typeof buffer === 'function') {
       callback = buffer
@@ -81,8 +122,6 @@ class Pack extends Readable {
     }
 
     if (!callback) callback = noop
-
-    const self = this
 
     if (!header.size || header.type === 'symlink') header.size = 0
     if (!header.type) header.type = modeToType(header.mode)
@@ -92,72 +131,44 @@ class Pack extends Readable {
     if (!header.mtime) header.mtime = new Date()
 
     if (typeof buffer === 'string') buffer = b4a.from(buffer)
+
+    const sink = new Sink(this, header, callback)
+
     if (b4a.isBuffer(buffer)) {
       header.size = buffer.byteLength
-      this._encode(header)
-      const ok = this.push(buffer)
-      overflow(self, header.size)
-      if (ok) process.nextTick(callback)
-      else this._drain = callback
-      return new Void()
+      sink.write(buffer)
+      sink.end()
+      return sink
     }
 
-    if (header.type === 'symlink' && !header.linkname) {
-      const linkSink = new LinkSink()
-      linkSink
-        .on('error', function (err) {
-          self.destroy()
-          callback(err)
-        })
-        .on('close', function () {
-          header.linkname = linkSink.linkname
-          self._encode(header)
-          callback()
-        })
-
-      return linkSink
+    if (sink._isVoid) {
+      sink.end()
+      return sink
     }
-
-    this._encode(header)
-
-    if (header.type !== 'file' && header.type !== 'contiguous-file') {
-      process.nextTick(callback)
-      return new Void()
-    }
-
-    const sink = new Sink(this)
-    sink
-      .on('error', function (err) {
-        self._stream = null
-        self.destroy()
-        callback(err)
-      })
-      .on('close', function () {
-        self._stream = null
-
-        if (sink.written !== header.size) { // corrupting tar
-        }
-
-        overflow(self, header.size)
-        if (self._finalizing) { self.finalize() }
-        callback()
-      })
-
-    this._stream = sink
 
     return sink
   }
 
   finalize () {
-    if (this._stream) {
+    if (this._stream || this._pending.length > 0) {
       this._finalizing = true
       return
     }
 
     if (this._finalized) return
     this._finalized = true
+
     this.push(END_OF_TAR)
     this.push(null)
+  }
+
+  _done (stream) {
+    if (stream !== this._stream) return
+
+    this._stream = null
+
+    if (this._finalizing) this.finalize()
+    if (this._pending.length) this._pending.shift()._continueOpen()
   }
 
   _encode (header) {
@@ -202,14 +213,55 @@ class Pack extends Readable {
     this.push(headers.encode(newHeader))
   }
 
-  _read (cb) {
+  _doDrain () {
     const drain = this._drain
     this._drain = noop
     drain()
+  }
+
+  _predestroy () {
+    const err = getStreamError(this)
+
+    if (this._stream) this._stream.destroy(err)
+
+    while (this._pending.length) {
+      const stream = this._pending.shift()
+      stream.destroy(err)
+      stream._continueOpen()
+    }
+
+    this._doDrain()
+  }
+
+  _read (cb) {
+    this._doDrain()
     cb()
   }
 }
 
 module.exports = function pack (opts) {
   return new Pack(opts)
+}
+
+function modeToType (mode) {
+  switch (mode & constants.S_IFMT) {
+    case constants.S_IFBLK: return 'block-device'
+    case constants.S_IFCHR: return 'character-device'
+    case constants.S_IFDIR: return 'directory'
+    case constants.S_IFIFO: return 'fifo'
+    case constants.S_IFLNK: return 'symlink'
+  }
+
+  return 'file'
+}
+
+function noop () {}
+
+function overflow (self, size) {
+  size &= 511
+  if (size) self.push(END_OF_TAR.subarray(0, 512 - size))
+}
+
+function mapWritable (buf) {
+  return b4a.isBuffer(buf) ? buf : b4a.from(buf)
 }
