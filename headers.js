14e710f55e7d494b6a48fe5226e31d489efd07bd
Mathias Buus <mathiasbuus@gmail.com>
2023-06-17 14:28:55 +0200

    update style in headers and inline binary strings

--- headers.js
+++ headers.js
@@ -3,166 +3,19 @@ const b4a = require('b4a')
 const ZEROS = '0000000000000000000'
 const SEVENS = '7777777777777777777'
 const ZERO_OFFSET = '0'.charCodeAt(0)
-const USTAR_MAGIC = b4a.from('ustar\x00', 'binary')
-const USTAR_VER = b4a.from('00', 'binary')
-const GNU_MAGIC = b4a.from('ustar\x20', 'binary')
-const GNU_VER = b4a.from('\x20\x00', 'binary')
+const USTAR_MAGIC = b4a.from([0x75, 0x73, 0x74, 0x61, 0x72, 0x00]) // ustar\x00
+const USTAR_VER = b4a.from([ZERO_OFFSET, ZERO_OFFSET])
+const GNU_MAGIC = b4a.from([0x75, 0x73, 0x74, 0x61, 0x72, 0x20]) // ustar\x20
+const GNU_VER = b4a.from([0x20, 0x00])
 const MASK = 0o7777
 const MAGIC_OFFSET = 257
 const VERSION_OFFSET = 263
 
-const clamp = function (index, len, defaultValue) {
-  if (typeof index !== 'number') return defaultValue
-  index = ~~index // Coerce to integer.
-  if (index >= len) return len
-  if (index >= 0) return index
-  index += len
-  if (index >= 0) return index
-  return 0
-}
-
-const toType = function (flag) {
-  switch (flag) {
-    case 0:
-      return 'file'
-    case 1:
-      return 'link'
-    case 2:
-      return 'symlink'
-    case 3:
-      return 'character-device'
-    case 4:
-      return 'block-device'
-    case 5:
-      return 'directory'
-    case 6:
-      return 'fifo'
-    case 7:
-      return 'contiguous-file'
-    case 72:
-      return 'pax-header'
-    case 55:
-      return 'pax-global-header'
-    case 27:
-      return 'gnu-long-link-path'
-    case 28:
-    case 30:
-      return 'gnu-long-path'
-  }
-
-  return null
-}
-
-const toTypeflag = function (flag) {
-  switch (flag) {
-    case 'file':
-      return 0
-    case 'link':
-      return 1
-    case 'symlink':
-      return 2
-    case 'character-device':
-      return 3
-    case 'block-device':
-      return 4
-    case 'directory':
-      return 5
-    case 'fifo':
-      return 6
-    case 'contiguous-file':
-      return 7
-    case 'pax-header':
-      return 72
-  }
-
-  return 0
-}
-
-const indexOf = function (block, num, offset, end) {
-  for (; offset < end; offset++) {
-    if (block[offset] === num) return offset
-  }
-  return end
-}
-
-const cksum = function (block) {
-  let sum = 8 * 32
-  for (let i = 0; i < 148; i++) sum += block[i]
-  for (let j = 156; j < 512; j++) sum += block[j]
-  return sum
-}
-
-const encodeOct = function (val, n) {
-  val = val.toString(8)
-  if (val.length > n) return SEVENS.slice(0, n) + ' '
-  else return ZEROS.slice(0, n - val.length) + val + ' '
-}
-
-/* Copied from the node-tar repo and modified to meet
- * tar-stream coding standard.
- *
- * Source: https://github.com/npm/node-tar/blob/51b6627a1f357d2eb433e7378e5f05e83b7aa6cd/lib/header.js#L349
- */
-function parse256 (buf) {
-  // first byte MUST be either 80 or FF
-  // 80 for positive, FF for 2's comp
-  let positive
-  if (buf[0] === 0x80) positive = true
-  else if (buf[0] === 0xFF) positive = false
-  else return null
-
-  // build up a base-256 tuple from the least sig to the highest
-  const tuple = []
-  let i
-  for (i = buf.length - 1; i > 0; i--) {
-    const byte = buf[i]
-    if (positive) tuple.push(byte)
-    else tuple.push(0xFF - byte)
-  }
-
-  let sum = 0
-  const l = tuple.length
-  for (i = 0; i < l; i++) {
-    sum += tuple[i] * Math.pow(256, i)
-  }
-
-  return positive ? sum : -1 * sum
-}
-
-const decodeOct = function (val, offset, length) {
-  val = val.slice(offset, offset + length)
-  offset = 0
-
-  // If prefixed with 0x80 then parse as a base-256 integer
-  if (val[offset] & 0x80) {
-    return parse256(val)
-  } else {
-    // Older versions of tar can prefix with spaces
-    while (offset < val.length && val[offset] === 32) offset++
-    const end = clamp(indexOf(val, 32, offset, val.length), val.length, val.length)
-    while (offset < end && val[offset] === 0) offset++
-    if (end === offset) return 0
-    return parseInt(val.slice(offset, end).toString(), 8)
-  }
-}
-
-const decodeStr = function (val, offset, length, encoding) {
-  return val.slice(offset, indexOf(val, 0, offset, offset + length)).toString(encoding)
-}
-
-const addLength = function (str) {
-  const len = b4a.byteLength(str)
-  let digits = Math.floor(Math.log(len) / Math.log(10)) + 1
-  if (len + digits >= Math.pow(10, digits)) digits++
-
-  return (len + digits) + str
-}
-
-exports.decodeLongPath = function (buf, encoding) {
+exports.decodeLongPath = function decodeLongPath (buf, encoding) {
   return decodeStr(buf, 0, buf.length, encoding)
 }
 
-exports.encodePax = function (opts) { // TODO: encode more stuff in pax
+exports.encodePax = function encodePax (opts) { // TODO: encode more stuff in pax
   let result = ''
   if (opts.name) result += addLength(' path=' + opts.name + '\n')
   if (opts.linkname) result += addLength(' linkpath=' + opts.linkname + '\n')
@@ -175,7 +28,7 @@ exports.encodePax = function (opts) { // TODO: encode more stuff in pax
   return b4a.from(result)
 }
 
-exports.decodePax = function (buf) {
+exports.decodePax = function decodePax (buf) {
   const result = {}
 
   while (buf.length) {
@@ -195,7 +48,7 @@ exports.decodePax = function (buf) {
   return result
 }
 
-exports.encode = function (opts) {
+exports.encode = function encode (opts) {
   const buf = b4a.alloc(512)
   let name = opts.name
   let prefix = ''
@@ -238,7 +91,7 @@ exports.encode = function (opts) {
   return buf
 }
 
-exports.decode = function (buf, filenameEncoding, allowUnknownFormat) {
+exports.decode = function decode (buf, filenameEncoding, allowUnknownFormat) {
   let typeflag = buf[156] === 0 ? 0 : buf[156] - ZERO_OFFSET
 
   let name = decodeStr(buf, 0, 100, filenameEncoding)
@@ -293,4 +146,151 @@ exports.decode = function (buf, filenameEncoding, allowUnknownFormat) {
     devmajor,
     devminor
   }
+}
+
+function clamp (index, len, defaultValue) {
+  if (typeof index !== 'number') return defaultValue
+  index = ~~index // Coerce to integer.
+  if (index >= len) return len
+  if (index >= 0) return index
+  index += len
+  if (index >= 0) return index
+  return 0
+}
+
+function toType (flag) {
+  switch (flag) {
+    case 0:
+      return 'file'
+    case 1:
+      return 'link'
+    case 2:
+      return 'symlink'
+    case 3:
+      return 'character-device'
+    case 4:
+      return 'block-device'
+    case 5:
+      return 'directory'
+    case 6:
+      return 'fifo'
+    case 7:
+      return 'contiguous-file'
+    case 72:
+      return 'pax-header'
+    case 55:
+      return 'pax-global-header'
+    case 27:
+      return 'gnu-long-link-path'
+    case 28:
+    case 30:
+      return 'gnu-long-path'
+  }
+
+  return null
+}
+
+function toTypeflag (flag) {
+  switch (flag) {
+    case 'file':
+      return 0
+    case 'link':
+      return 1
+    case 'symlink':
+      return 2
+    case 'character-device':
+      return 3
+    case 'block-device':
+      return 4
+    case 'directory':
+      return 5
+    case 'fifo':
+      return 6
+    case 'contiguous-file':
+      return 7
+    case 'pax-header':
+      return 72
+  }
+
+  return 0
+}
+
+function indexOf (block, num, offset, end) {
+  for (; offset < end; offset++) {
+    if (block[offset] === num) return offset
+  }
+  return end
+}
+
+function cksum (block) {
+  let sum = 8 * 32
+  for (let i = 0; i < 148; i++) sum += block[i]
+  for (let j = 156; j < 512; j++) sum += block[j]
+  return sum
+}
+
+function encodeOct (val, n) {
+  val = val.toString(8)
+  if (val.length > n) return SEVENS.slice(0, n) + ' '
+  else return ZEROS.slice(0, n - val.length) + val + ' '
+}
+
+/* Copied from the node-tar repo and modified to meet
+ * tar-stream coding standard.
+ *
+ * Source: https://github.com/npm/node-tar/blob/51b6627a1f357d2eb433e7378e5f05e83b7aa6cd/lib/header.js#L349
+ */
+function parse256 (buf) {
+  // first byte MUST be either 80 or FF
+  // 80 for positive, FF for 2's comp
+  let positive
+  if (buf[0] === 0x80) positive = true
+  else if (buf[0] === 0xFF) positive = false
+  else return null
+
+  // build up a base-256 tuple from the least sig to the highest
+  const tuple = []
+  let i
+  for (i = buf.length - 1; i > 0; i--) {
+    const byte = buf[i]
+    if (positive) tuple.push(byte)
+    else tuple.push(0xFF - byte)
+  }
+
+  let sum = 0
+  const l = tuple.length
+  for (i = 0; i < l; i++) {
+    sum += tuple[i] * Math.pow(256, i)
+  }
+
+  return positive ? sum : -1 * sum
+}
+
+function decodeOct (val, offset, length) {
+  val = val.slice(offset, offset + length)
+  offset = 0
+
+  // If prefixed with 0x80 then parse as a base-256 integer
+  if (val[offset] & 0x80) {
+    return parse256(val)
+  } else {
+    // Older versions of tar can prefix with spaces
+    while (offset < val.length && val[offset] === 32) offset++
+    const end = clamp(indexOf(val, 32, offset, val.length), val.length, val.length)
+    while (offset < end && val[offset] === 0) offset++
+    if (end === offset) return 0
+    return parseInt(val.slice(offset, end).toString(), 8)
+  }
+}
+
+function decodeStr (val, offset, length, encoding) {
+  return val.slice(offset, indexOf(val, 0, offset, offset + length)).toString(encoding)
+}
+
+function addLength (str) {
+  const len = b4a.byteLength(str)
+  let digits = Math.floor(Math.log(len) / Math.log(10)) + 1
+  if (len + digits >= Math.pow(10, digits)) digits++
+
+  return (len + digits) + str
 }
