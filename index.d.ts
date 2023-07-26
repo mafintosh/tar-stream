a51277e2ac7120a6a2e7086449b538f100e20a37
Alvis HT Tang <alvis@hilbert.space>
2023-07-26 04:42:21 +0100

    fix: remove unnecessary namespace

--- index.d.ts
+++ index.d.ts
@@ -1,68 +1,66 @@
-declare namespace tarstream {
-  interface Header {
-    name: string
-    size: number
-    mode: number
-    mtime: Date
-    type: 'file' | 'link' | 'symlink' | 'directory' | 'block-device' | 'character-device' | 'fifo' | 'contiguous-file'
-    linkname: string
-    uid: number
-    gid: number
-    uname: string
-    gname: string
-    devmajor: number
-    devminor: number
-    pax?: object
-  }
-
-  type HeaderForPack = Partial<Header> & {
-    name: string
-  }
-
-  interface Pack extends NodeJS.ReadableStream {
-    entry(header: HeaderForPack, callback?: (error?: Error) => void): Sink
-    entry(
-      header: HeaderForPack,
-      buffer: string | Buffer,
-      callback?: (error?: Error) => void
-    ): Sink
-    destroy(): void
-    finalize(): void
-  }
-
-  interface Sink extends NodeJS.WritableStream {
-  }
-
-  interface ExtractOptions {
-    filenameEncoding?: string
-  }
-
-  interface Source extends NodeJS.ReadableStream { 
-    header: Header
-    offset: number
-  }
-
-  interface Extract extends NodeJS.WritableStream {
-    on(
-      event: 'entry',
-      callback: (
-        header: Header,
-        stream: Source,
-        next: () => void
-      ) => void
-    ): void
-    on(event: 'close', callback: () => void)
-    on(event: 'finish', callback: () => void)
-    on(event: 'error', callback: (error: Error) => void)
-    destroy(error?: Error): void
-    [Symbol.asyncIterator](): AsyncIterableIterator<Source>
-  }
-
-  interface Module {
-    pack(): Pack
-    extract(options?: ExtractOptions): Extract
-  }
-
-  export function pack(): Pack
-  export function extract(options?: ExtractOptions): Extract
+interface Header {
+  name: string
+  size: number
+  mode: number
+  mtime: Date
+  type: 'file' | 'link' | 'symlink' | 'directory' | 'block-device' | 'character-device' | 'fifo' | 'contiguous-file'
+  linkname: string
+  uid: number
+  gid: number
+  uname: string
+  gname: string
+  devmajor: number
+  devminor: number
+  pax?: object
 }
+
+type HeaderForPack = Partial<Header> & {
+  name: string
+}
+
+interface Pack extends NodeJS.ReadableStream {
+  entry(header: HeaderForPack, callback?: (error?: Error) => void): Sink
+  entry(
+    header: HeaderForPack,
+    buffer: string | Buffer,
+    callback?: (error?: Error) => void
+  ): Sink
+  destroy(): void
+  finalize(): void
+}
+
+interface Sink extends NodeJS.WritableStream {
+}
+
+interface ExtractOptions {
+  filenameEncoding?: string
+}
+
+interface Source extends NodeJS.ReadableStream { 
+  header: Header
+  offset: number
+}
+
+interface Extract extends NodeJS.WritableStream {
+  on(
+    event: 'entry',
+    callback: (
+      header: Header,
+      stream: Source,
+      next: () => void
+    ) => void
+  ): void
+  on(event: 'close', callback: () => void)
+  on(event: 'finish', callback: () => void)
+  on(event: 'error', callback: (error: Error) => void)
+  destroy(error?: Error): void
+  [Symbol.asyncIterator](): AsyncIterableIterator<Source>
+}
+
+interface Module {
+  pack(): Pack
+  extract(options?: ExtractOptions): Extract
+}
+
+export function pack(): Pack
+export function extract(options?: ExtractOptions): Extract
