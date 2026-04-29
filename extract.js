cca2c77781fd9fd1f36e86cea703e570d6c77e0d
Doug Mayer <me@doxavore.com>
2026-04-29 06:17:01 -0500

    Fix typo in BufferList.shiftFirst. (#171)

--- extract.js
+++ extract.js
@@ -21,7 +21,7 @@ class BufferList {
   }
 
   shiftFirst (size) {
-    return this._buffered === 0 ? null : this._next(size)
+    return this.buffered === 0 ? null : this._next(size)
   }
 
   shift (size) {
